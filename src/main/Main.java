b2c0c93747737cd301f219802eb5a5c68cfef096
Carolina2032 <69551561+Carolina2032@users.noreply.github.com>
2020-08-18 21:38:13 -0500

    Comentario sobre la actualizacion registrada en repositorio local

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 import controller.Controller;
+import model.data_structures.ArregloDinamico;
 
 public class Main {
 	
@@ -7,5 +8,29 @@ public class Main {
 	{
 		Controller controler = new Controller();
 		controler.run();
+		
+		ArregloDinamico<Integer> intArray = new ArregloDinamico<>();
+		
+		intArray.agregar(2);
+		intArray.agregar(5);
+		intArray.agregar(8);
+		intArray.agregar(10);
+		intArray.agregar(15);
+		intArray.agregar(21);
+		intArray.agregar(25);
+		intArray.agregar(35);
+		intArray.agregar(48);
+		
+	for (int i= 0; i < intArray.darCapacidad(); i++) {
+		System.out.println(intArray.darElemento(i));
+		
+		
+	}
+				
+			
+		
+	
+		
+		
 	}
 }
